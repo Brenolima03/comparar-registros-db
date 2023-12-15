40f8e155cf8032524db5f26f58eddf47f8b6c1fd
Brenoclima03 <brenocslima@outlook.com>
2023-12-15 10:13:45 -0400

    retornando apenas a lista de notas faltantes

--- consultar_notas_jde_compliance.py
+++ consultar_notas_jde_compliance.py
@@ -2,56 +2,59 @@ import cx_Oracle
 import os
 from dotenv import load_dotenv
 
+# Função para criar uma conexão com o Oracle.
 def criar_conexao_oracle(usuario, senha, tns_name):
     try:
-        # Tenta estabelecer uma conexão Oracle usando cx_Oracle
+        # Tenta estabelecer a conexão.
         conexao = cx_Oracle.connect(f'{usuario}/{senha}@{tns_name}')
         return conexao
     except cx_Oracle.Error as e:
-        # Lança uma exceção com uma mensagem personalizada se houver um erro
+        # Levanta uma exceção se houver um erro ao conectar.
         raise ValueError(f"Erro ao conectar ao Oracle: {e}")
 
+# Função para executar uma consulta e formatar o resultado.
 def executar_query(conexao, query):
-    resultados = []
     with conexao.cursor() as cursor:
-        # Executa a consulta SQL usando o cursor
+        # Executa a consulta.
         cursor.execute(query)
-        # Obtém todos os resultados da consulta
-        resultados = cursor.fetchall()
-    return resultados
+        # Obtém a primeira linha do resultado.
+        resultado = cursor.fetchone()
+        if resultado is not None:
+            # Formatando o retorno para remover os espaços e transformar em tupla.
+            resultado = tuple(map(str.strip, resultado))
+    return resultado
 
+# Função para comparar notas entre dois sistemas.
 def comparar_notas(conexao, lista_de_notas, tabela, campo_nota, campo_serie):
+    notas_faltando_jde = []
     for nota in lista_de_notas:
-        # Extrai o número da nota e a série
-        numero_nota = nota[:-3] if '/' in nota[:-2] else nota[:-2]
-        serie = nota[-2:] if '/' in nota[:-2] else nota[-1]
-
-        # Constrói a consulta SQL usando formatação de string
-        query = f"SELECT {campo_nota} FROM {tabela} WHERE {campo_nota} = '{numero_nota}' AND {campo_serie} = '{serie}'"
+        # Formatando tanto o número quanto a série da nota para definir onde termina ou começa cada um, dependendo de onde está o '/'.
+        query = f"SELECT {campo_nota} FROM {tabela} WHERE {campo_nota} = '{nota[:-3] if '/' in nota[:-2] else nota[:-2]}' \
+        AND {campo_serie} = '{nota[-2:] if '/' in nota[:-2] else nota[-1]}'"
         
         try:
-            # Executa a consulta usando a função definida anteriormente
-            resultados = executar_query(conexao, query)
-            if not resultados:
-                print(f"A nota {nota} não está presente em {tabela}.")
-            else:
-                print(f"A nota {nota} está presente em {tabela}.")
+            # Executa a consulta e verifica se há resultado.
+            resultado = executar_query(conexao, query)
+            if not resultado:
+                # Adiciona a nota à lista se não houver resultado.
+                notas_faltando_jde.append(nota)
         except Exception as e:
-            # Manipula exceções durante a execução da consulta
             print(f"Erro ao executar a consulta para a nota {nota}: {str(e)}")
 
-# Carrega o ambiente virtual
+    return notas_faltando_jde
+
 with open('notas_para_comparar.txt', 'r') as notas_para_comparar:
-    # Lê as notas do arquivo e cria uma lista
+    # Lê as notas do arquivo e remove espaços em branco.
     lista_de_notas = [linha.strip() for linha in notas_para_comparar]
 
-# Carregar variáveis de ambiente do arquivo .env
+# Carrega variáveis de ambiente do arquivo .env
 load_dotenv()
 
-# Cria a conexão Oracle usando as variáveis de ambiente
+# Cria a conexão Oracle usando as variáveis de ambiente.
 with criar_conexao_oracle(os.getenv("ORACLE_USUARIO"), os.getenv("ORACLE_SENHA"), os.getenv("ORACLE_TNS_NAME")) as conexao:
-    # Consulta para o Compliance
-    comparar_notas(conexao, lista_de_notas, 'CSF_OWN.NOTA_FISCAL', 'NRO_NF', 'SERIE')
+    # Compara as notas entre os sistemas
+    notas_faltando_jde = comparar_notas(conexao, lista_de_notas, 'DBRDTA.f47011', 'SYCACT', 'SYAUTN')
 
-    # Consulta para o JDE
-    comparar_notas(conexao, lista_de_notas, 'DBRDTA.f47011', 'SYCACT', 'SYAUTN')
+print("Notas faltando no JDE:")
+for nota in notas_faltando_jde:
+    print(nota)
